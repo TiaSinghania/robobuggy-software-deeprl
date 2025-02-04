54016507fc193e8a419d67a542df04681bf835af
Evan <ebfoster111@gmail.com>
2025-02-04 14:03:14 -0500

    Made better watchdog 🐶🔎 with error types (#19)

--- rb_ws/src/buggy/scripts/watchdog/watchdog_node.py
+++ rb_ws/src/buggy/scripts/watchdog/watchdog_node.py
@@ -2,45 +2,62 @@
 
 import rclpy
 from rclpy.node import Node
+from rclpy.subscription import Subscription
+from rclpy.publisher import Publisher
 
-from std_msgs.msg import Bool
+from std_msgs.msg import Int32, Int8
+
+from util.errors import BuggyError
+
 
 class Watchdog(Node):
 
-    def __init__(self):
-        """
-        Constructor for Watchdog class.
+    curr_errors: BuggyError = BuggyError(0)
+    error_subscriber: Subscription
+    led_publisher: Publisher
 
-        Creates a ROS node with a publisher that periodically sends a message
-        indicating whether the node is still alive.
+    def __init__(self):
+        """ Constructor for Watchdog class.
         
         """
         super().__init__('watchdog')
 
-        # Publishers
-        self.heartbeat_publisher = self.create_publisher(Bool, 'self/debug/heartbeat', 1)
+        self.error_subscriber = self.create_subscription(
+            Int32,
+            BuggyError.ros_topic_name,
+            self.error_callback,
+            10
+        )
 
-        # Subscribers
-        self.heartbeat_subscriber = self.create_subscription(Bool, 'self/debug/heartbeat', self.heartbeat_listener, 1)
+        self.led_publisher = self.create_publisher(
+            Int32,
+            "/input/sanity_warning",
+            10
+        )
 
-        timer_period = 0.01  # seconds (100 Hz)
-        self.timer = self.create_timer(timer_period, self.loop)
-        self.i = 0 # Loop Counter
+    def update_status_led(self):
+        # Errors
+        if BuggyError.REALLY_FREAKING_BAD in self.curr_errors\
+                or BuggyError.PATH_PLANNING_FAULT in self.curr_errors\
+                or BuggyError.STANLEY_CRAPPED_ITSELF in self.curr_errors:
+            self.led_publisher.publish(Int8(data=2))
+        # Warnings
+        elif BuggyError.LOW_BATTERY in self.curr_errors\
+                or BuggyError.SENSOR_UNAVAILABLE in self.curr_errors\
+                or BuggyError.VISION_UNAVAILABLE in self.curr_errors\
+                or BuggyError.INTERBUGGY_COMMUNICATION_LOST in self.curr_errors\
+                or BuggyError.GPS_UNAVAILABLE in self.curr_errors:
+            self.led_publisher.publish(Int8(data=1))
+        # All good
+        else:
+            self.led_publisher.publish(Int8(data=0))
 
-    def loop(self):
-        # Loop for the code that operates every 10ms
-        msg = Bool()
-        msg.data = True
-        self.heartbeat_publisher.publish(msg)
-
-    def heartbeat_listener(self, msg : Node):
-        """
-        Subscriber Function that checks if Heartbeat is ever alse
-        It never actually is false, this is just a demonstration of a subscriber
-        If it ever actually is false, something cursed has happened
-        """
-        if msg.data == False:
-            self.get_logger().error("Hearbeat Failed!")
+    def error_callback(self, msg: Int32):
+        if msg.data:
+            curr = self.curr_errors
+            self.curr_errors |= BuggyError(msg.data)
+            if curr != self.curr_errors:
+                self.update_status_led()
 
 
 def main(args=None):
